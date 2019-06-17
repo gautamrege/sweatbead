1e4e96e5b0fa4fbae0e846cd7305cb0411db0a5f
Gautam Rege <gautam@joshsoftware.com>
2019-06-17 11:43:49 +0530

    Section 1.3 Refactoring the microservice

--- samplemgr/go.mod
+++ samplemgr/go.mod
@@ -1,8 +1,21 @@
 module github.com/gautamrege/packt/sweatbead/samplemgr
 
 require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gorilla/mux v1.7.0
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/mattn/go-sqlite3 v1.10.0
 	github.com/urfave/negroni v1.0.0
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.0.3
+	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
+
+replace (
+	github.com/gautamrege/packt/sweatbead/samplemgr/db => /Users/gautam/work/sweatbead/samplemgr/db
+	github.com/gautamrege/packt/sweatbead/samplemgr/service => /Users/gautam/work/sweatbead/samplemgr/service
 )
--- samplemgr/samplemgr.go
+++ samplemgr/samplemgr.go
@@ -1,32 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"strconv"
-	"time"
 
-	"github.com/gorilla/mux"
-	"github.com/jmoiron/sqlx"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/gautamrege/packt/sweatbead/samplemgr/db"
+	"github.com/gautamrege/packt/sweatbead/samplemgr/service"
+
 	"github.com/urfave/negroni"
-)
-
-var (
-	db *sqlx.DB
 )
 
 func main() {
 
 	// Initialize db
-	err := initDB()
+	err, db := db.GetDB()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Db Initialized: ", db)
 
 	// mux router
-	router := initRouter()
+	router := service.InitRouter()
 
 	// init web server
 	server := negroni.Classic()
@@ -37,48 +31,4 @@ func main() {
 
 	server.Run(addr)
 
-}
-
-/* Initialize the database */
-func initDB() (err error) {
-	db, err := sqlx.Connect("sqlite3", "__deleteme.db")
-	if err != nil {
-		return
-	}
-
-	if err = db.Ping(); err != nil {
-		return
-	}
-
-	db.SetMaxIdleConns(5)
-	db.SetMaxOpenConns(5)
-	db.SetConnMaxLifetime(time.Duration(30) * time.Minute)
-
-	return
-}
-
-/* The routing mechanism. Mux helps us define handler functions and the access methods */
-func initRouter() (router *mux.Router) {
-	router = mux.NewRouter()
-	router.HandleFunc("/ping", pingHandler).Methods(http.MethodGet)
-	return
-}
-
-type Response struct {
-	Message string `json:"message"`
-}
-
-/* Each handler will then respond with marshalled Json data */
-func pingHandler(rw http.ResponseWriter, req *http.Request) {
-	response := Response{Message: "pong"}
-
-	status := http.StatusOK
-	respBytes, err := json.Marshal(response)
-	if err != nil {
-		panic(err)
-		status = http.StatusInternalServerError
-	}
-	rw.Header().Add("Content-Type", "application/json")
-	rw.WriteHeader(status)
-	rw.Write(respBytes)
 }
